66d2b13f8f23e011e15f597644cff8997e74ed79
nemoyuan <nemoyuan@zego.im>
2021-01-03 14:33:27 +0800

    lc2

--- leetcode-vscode/2.两数相加.cpp
+++ leetcode-vscode/2.两数相加.cpp
@@ -13,7 +13,8 @@
  * Total Submissions: 1.7M
  * Testcase Example:  '[2,4,3]\n[5,6,4]'
  *
- * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 给出两个 非空
+ * 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
  *
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  *
@@ -39,20 +40,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
- public:
-  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-  {
-    int        cur_sum = 0;
-    int        cur_c   = 0;
-    ListNode*  result  = nullptr;
-    ListNode** cur     = &result;
+#define _val(node) (node ? (node->val) : 0)
+class Solution {
+public:
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    int cur_sum = 0;
+    int cur_c = 0;
+    ListNode *result = nullptr;
+    ListNode **cur = &result;
 
-    // auto _val  = [](const ListNode* node) { return node ? (node->val) : 0; };
-    // auto _push = [&]() {l1 = l1 ? l1->next : l1; l2 = l2 ? l2->next : l2; };
-    #define _val(node)  (node ? (node->val) : 0)
-    #define _push() l1 = (l1 ? l1->next : l1); l2 = (l2 ? l2->next : l2);
 
     while (l1 || l2 || cur_c) {
       cur_sum = _val(l1) + _val(l2) + cur_c;
@@ -63,10 +59,10 @@ class Solution
 
       cur = &((*cur)->next);
 
-      _push();
+      l1 = (l1 ? l1->next : l1);
+      l2 = (l2 ? l2->next : l2);
     }
     return result;
   }
 };
 // @lc code=end
-
